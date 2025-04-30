0ed20e38fc4321aacfeb65a7afa41a7ee255aace
Laura Jane Antunes <laura.antunes@gmail.com>
2025-04-29 21:37:08 -0300

    alt ip da maquina e desinstalado typescript

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // config.js
 // Mude o IP para o IP do seu computador na rede local
-export const API_BASE_URL = 'http://192.168.0.100:3000';
+export const API_BASE_URL = 'http://192.168.56.1:3000';
+// 'http://192.168.0.100:3000';
